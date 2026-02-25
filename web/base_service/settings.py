92079eddb9a40c5c39ab4d1a7eb3a826c5bb593e
alejandrogm90 <alejandrogomezmartin90@outlook.com>
2026-02-25 09:32:07 +0100

    + Add test hosts

--- web/base_service/settings.py
+++ web/base_service/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = secrets.token_hex(32)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.200', 'localhost']
 
 # Application definition
 
